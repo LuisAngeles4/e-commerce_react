39d5c9de8e393c4ab65a11b9b54c780bc37f4062
LuisAngeles4 <neto19041@gmail.com>
2021-10-04 12:53:08 -0500

    problema en quantity

---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const urlApiProducts = "http://myjson.dit.upm.es/api/bins/1ta9"; // https://myjson.dit.upm.es/1ta9
+export const BASE_PATH = "http://localhost:3000";
+export const STORAGE_PRODUCTS_CART = "productsCart";
